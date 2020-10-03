d94db333c3ac7da06cd443b27d3b028a6bde234c
Daniikk-1012 <daniyarsar@gmail.com>
2020-10-03 08:59:29 +0600

    Generate LibGDX project

---
+++ desktop/src/com/wgsoft/game/timesaver/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wgsoft.game.timesaver.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wgsoft.game.timesaver.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
